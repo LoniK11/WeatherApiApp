561c046c4902ceacaa51ecfdecf337b2027082c5
Valon Kulici <valonkulici11@gmail.com>
2025-06-03 18:09:16 +0200

    Created Weather service and configure API URL and key

---
+++ src/main/java/com/example/WeatherApp/Weather/WeatherService.java
@@ -0,0 +1,13 @@
+package com.example.WeatherApp.Weather;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+    @Value("weather.api.key")
+    private String weatherApiKey;
+
+    private final String weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+}
