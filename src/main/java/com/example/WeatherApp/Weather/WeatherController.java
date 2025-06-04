1a743ff75b92157a63d9dcabeca4c8220340e257
Valon Kulici <valonkulici11@gmail.com>
2025-06-04 15:04:07 +0200

    Created GET method to get a city weather using WeatherResponse class

--- src/main/java/com/example/WeatherApp/Weather/WeatherController.java
+++ src/main/java/com/example/WeatherApp/Weather/WeatherController.java
@@ -1,5 +1,7 @@
 package com.example.WeatherApp.Weather;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class WeatherController {
 
     public WeatherController(WeatherService weatherService){
         this.weatherService = weatherService;
+    }
+
+    @GetMapping("/{city}")
+    public WeatherResponse getWeatherByCity(@PathVariable("city") String city){
+        return this.weatherService.getWeatherByCity(city);
     }
 }
--- src/main/java/com/example/WeatherApp/Weather/WeatherService.java
+++ src/main/java/com/example/WeatherApp/Weather/WeatherService.java
@@ -2,6 +2,7 @@ package com.example.WeatherApp.Weather;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
@@ -11,4 +12,10 @@ public class WeatherService {
 
     private final String weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 
+    public WeatherResponse getWeatherByCity(String city){
+        String url = this.weatherUrl + "?q=" + city + "&appid=" + this.weatherApiKey + "&units=metric";
+
+        WeatherResponse response = new RestTemplate().getForObject(url,WeatherResponse.class);
+        return response;
+    }
 }
